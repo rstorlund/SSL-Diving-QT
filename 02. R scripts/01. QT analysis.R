32e73c144518bcb7c06aa5697c99b151537bf946
Rhea Storlund <rstorlund@gmail.com>
2024-06-10 17:33:42 -0700

    Read in the data

---
+++ 02. R scripts/01. QT analysis.R
@@ -0,0 +1,12 @@
+#####QT Analysis#####
+#This file plots QT intervals over time for
+#Steller sea lions performing stationary dives
+#Aligns the HR profiles from the start of the dive
+#and plots them altogether
+#then calculates average change in HR over the dive
+
+#####Load libraries#####
+#####Read in the data#####
+#####Plot HR over time#####
+#####Summary stats#####
+#####Statistics######
