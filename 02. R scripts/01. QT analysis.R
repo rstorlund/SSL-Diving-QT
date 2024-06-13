135d3571929f3a0366b88060736532810cd8b52a
Rhea Storlund <rstorlund@gmail.com>
2024-06-12 17:34:51 -0700

    read in data

--- 02. R scripts/01. QT analysis.R
+++ 02. R scripts/01. QT analysis.R
@@ -6,7 +6,27 @@
 #then calculates average change in HR over the dive
 
 #####Load libraries#####
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+
 #####Read in the data#####
+
+#create a list of files
+mydir <- '/Users/rhea/Library/CloudStorage/OneDrive-UBC/PhD/QT/Analysis/SSL-Diving-QT/01. Raw data'
+myfiles <- list.files(path=mydir, pattern="*.csv", full.names=TRUE)
+datalist = list()
+
+#read in files
+for (i in 1:length(myfiles)) {
+  #read in file
+  df <- read.csv(file = myfiles[1],
+                 sep = ",",
+                 stringsAsFactors = FALSE,
+                 strip.white = TRUE,
+                 na.strings = c("NA",""))
+  
+
 #####Plot HR over time#####
 #####Summary stats#####
 #####Statistics######
